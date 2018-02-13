1a140697cda30334738d9ea7548bd4ed3c6cb295
Hari Lamichhane <onwithhari@gmail.com>
2018-02-13 12:59:23 +0545

    Init project

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Landing extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>GCES IT Expo</h1>
+      </div>
+    );
+  }
+}
+
+export default Landing;
