e22b991a035f0afd0c94da8b599d3d9f0e51c5d3
Hari Lamichhane <onwithhari@gmail.com>
2018-02-19 11:56:06 +0545

    Separate header.

---
+++ src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Level, Section } from 'reactbulma';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+
+  render() {
+    return (
+      <header>
+        <div style={{ maxWidth: '200px', margin: '0 auto' }}>
+          <Level mobile>
+            <Level.Left>
+              <Link to='/about'>About</Link>
+            </Level.Left>
+            <Level.Item hasTextCentered>
+              <Link to="/">
+                <img src={require('../resources/itexpo_logo.png')} alt="IT Expo" style={{height: '48px'}} />
+              </Link>
+            </Level.Item>
+            <Level.Right>
+              <a href='mailto:expo@gces.edu.np?subject=About+IT+Expo'>Contact</a>
+            </Level.Right>
+          </Level>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
