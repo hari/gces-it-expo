55ce6074dbe4ef8023e93d4e5d56282029601370
Hari Lamichhane <onwithhari@gmail.com>
2018-02-19 11:49:40 +0545

    Show the action if available in event.

---
+++ src/components/Slide.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class Slide extends React.Component {
+
+  render() {
+    const { event, active } = this.props;
+    return (
+      <div className={'carousel-item' + (active ? ' active' : '')}>
+        <div style={{ textAlign: 'center', padding: '8px' }}>
+          <img style={{ margin: '16px' }} src={event.image} />
+        </div>
+        <div className="card-body" style={{ background: '#0003', color: '#fff', padding: '16px' }}>
+         <h1 className="card-title" style={{ fontSize: '24px', fontWeight: 900 }}>{ event.name }</h1>
+         <div style={{ paddingBottom: '24px' }}>
+          <p>{ event.description }</p>
+          {
+            event.action && <div style={{ marginTop: '16px' }} dangerouslySetInnerHTML={{ __html: event.action }} />
+          }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Slide;
