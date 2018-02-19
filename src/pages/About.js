89d95f58f37d2fbc72bf8420e7c8ebb5a99d7022
Hari Lamichhane <onwithhari@gmail.com>
2018-02-19 11:49:27 +0545

    Create contact page.

---
+++ src/pages/About.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+import Header from '../components/Header';
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <div id="about">
+        <Header />
+        <div className="container gces-col">
+          <h1>GCES IT EXPO 2018</h1>
+          <p>
+            Gandaki College of Engineering and Science (GCES) is organizing "GCES IT EXPO 2018" for two days on 25th and 26th February 2018 (13th to 14th Falgun 2074) in the college premises itself at Lamachaur, Pokhara. GCES IT EXPO is all about Information and Communication Technology (ICT) exhibitions and awareness program.  The exhibition is aimed at creating awareness about Information and Communication Technologies and newer and emerging tools of IT industry, Free/Open Source Software (FOSS) and how they can fit in our society and how can we benefit from IT in organizations, government offices, educational institutions and our day to day life.  It will address many more challenges that our society has been facing without IT.
+          </p>
+          <p>
+            ICT presents itself as an effective and affordable solution to overcome the socio-economic imbalance due to the geographical remoteness of Nepal. But due to lack of awareness, these technologies are not exploited. GCES promotes ICT by showcasing the latest hardware and software products, Free Software Projects, and other related Technologies.
+          </p>
+          <p>
+            The IT EXPO will attract Secondary School students, 10+2 students, college and university students, entrepreneurs, professionals, enthusiasts and the IT business personals as well as general public. The major highlights of the exhibition will be Technology Demonstrations, Technical Talks, Documentaries, Software Development Contests, Quiz Competitions, Problem solving competitions and so on.
+          </p>
+          <hr />
+          <h1>COMMITTEE MEMBERS</h1>
+          <div className="row">
+            <div className="col">
+             <h3>Faculty Committee</h3>
+             <label>Chairman:</label>
+              <ul>
+                <li>Sanjay Niroula</li>
+              </ul>
+             <label>Members:</label>
+              <ul>
+                <li>Ashok Raj Parajuli</li>
+                <li>Sujan Tamrakar</li>
+                <li>Rajendra Thapa</li>
+                <li>Mahesh Shakya</li>
+                <li>Sanjita Lamichhane</li>
+                <li>Jhalak Lamichhane</li>
+                <li>Shobha KC</li>
+                <li>Rukmina Pageni</li>
+              </ul>
+            </div>
+            <div className="col">
+             <h3>Student Committee</h3>
+             <label>President:</label>
+             <ul>
+                <li>Sudha Shrestha</li>
+             </ul>
+             <label>Vice president:</label>
+             <ul>
+                <li>Suraj Shrestha</li>
+             </ul>
+             <label>Secretary:</label>
+             <ul>
+                <li>Parash Devkota</li>
+             </ul>
+             <label>Co-secretary:</label>
+             <ul>
+                <li>Orjina Kafle</li>
+             </ul>
+             <label>Treasurers:</label>
+             <ul>
+                <li>Saurav Sharma</li>
+             </ul>
+             <label>Members:</label>
+              <ul>
+                <li>Alina Timilsina</li>
+                <li>Roshan Adhikari</li>
+                <li>Swoichha Adhikari</li>
+                <li>Ashish Shrestha</li>
+                <li>Anchal Gurung</li>
+                <li>Hari Lamichhane</li>
+                <li>Ujjwal Poudel</li>
+                <li>Saurab Raj Upadhyaya</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default About;
