6dfe7db5f4ae78b4127653834d8b43b602976975
Hari Lamichhane <onwithhari@gmail.com>
2018-02-20 19:53:21 +0545

    Create store for downloadable content.

---
+++ src/data/Download.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    title: 'Project Expo entry form',
+    link: 'https://drive.google.com/open?id=1zKsMaR5T1MrlXb3c_F-0MNVsbuuxJcoL',
+    description: 'Form to be filled for registration.'
+  },
+  {
+    title: 'Project Expo rules',
+    link: 'https://drive.google.com/open?id=1HYOqv33DKOY_j5Mh-GXP1BOkqfCjT_Vv',
+    description: 'Rules to be followed by the participants.'
+  }
+]
