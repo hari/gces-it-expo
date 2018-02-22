116133fc78de41325f3b788b7e04c5351bfd3332
Hari Lamichhane <onwithhari@gmail.com>
2018-02-22 14:16:20 +0545

    Add new downloadable content.

--- src/data/Download.js
+++ src/data/Download.js
@@ -8,5 +8,30 @@ export default [
     title: 'Project Expo rules',
     link: 'https://drive.google.com/open?id=1HYOqv33DKOY_j5Mh-GXP1BOkqfCjT_Vv',
     description: 'Rules to be followed by the participants.'
+  },
+  {
+    title: 'Game of Codes — MCQ Session',
+    link: 'https://drive.google.com/open?id=0BzhoYp438QpOX3RuZ2JxUzlLd2Q0R1hwMk1Mb3BQUXBtOEZF',
+    description: 'Form for Game of Codes — MCQ Session'
+  },
+  {
+    title: 'Game of Codes — Coding Session',
+    link: 'https://drive.google.com/open?id=0BzhoYp438QpOTjNZZ25oVnpZakNuYkFmNTRwcXNDbjJOeUEw',
+    description: 'Form for Game of Codes — Coding Session'
+  },
+  {
+    title: 'Game of Code - Rules',
+    link: 'https://drive.google.com/open?id=0BzhoYp438QpON2I4TGpqM1RKLXN1OWtUMS1TQ2dJRjRQX2lV',
+    description: 'Rules for Game of Code'
+  },
+  {
+    title: 'CS GO Form',
+    link: 'https://docs.google.com/forms/d/e/1FAIpQLSdxTNhNnTu3p6bqvsZkkFmKoKXWDGaM7VzEWLZDCMJSWpO5zA/viewform',
+    description: 'Registration form for CS GO'
+  },
+  {
+    title: 'CS:GO Rules',
+    link: 'https://drive.google.com/file/d/0B5tuURNcF8ivdlA3VEVDM01QdThjd2NVU1ZMcl9KOU94dlFR/view',
+    description: 'Rules for CS GO'
   }
 ]
